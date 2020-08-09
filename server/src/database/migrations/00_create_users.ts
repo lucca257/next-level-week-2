5c145b2d5c2fb35a4a2419b6b3831c8dfd6d25ac
John Doe <johndoe@example.com>
2020-08-09 20:56:32 -0300

    base server api

---
+++ server/src/database/migrations/00_create_users.ts
@@ -0,0 +1,15 @@
+import knex from 'knex';
+
+export async function up(Knex: knex) {
+  return Knex.schema.createTable('users', table => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('avatar').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('bio').notNullable();
+  });
+}
+
+export async function down(knex: knex) {
+  return knex.schema.dropSchema('users');
+}
---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+
+//convertendo dados recebidos do body em json para objeto
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+  return response.json(request.body);
+});
+
+app.listen(3333);
